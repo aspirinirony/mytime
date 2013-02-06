aa5ad6f6a8b75c029e1e055fcca26357900d1e21
aspirinirony <aspirinirony@gmail.com>
2013-02-07 03:24:29 +0900

    index page

---
+++ index.php
@@ -0,0 +1,55 @@
+﻿<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <meta name="format-detection" content="telephone=no" />
+
+  <meta name="keywords" content="aspirinirony의 사이트" /> 
+  <meta name="description" content="aspirinirony의 사이트" />
+  <title>aspirinirony</title>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+        <script type="text/javascript">
+            $(document).on("mobileinit", function () {
+                $.mobile.defaultPageTransition = 'slide';
+            });
+        </script>
+  <script type="text/javascript" src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.js"></script>
+  <style>
+	*{
+		/*font-size: 14px;*/
+		line-height: 22px;
+		color: #222;
+		word-wrap: break-word;
+		word-break: break-all;
+		font-family: "나눔고딕","NanumGothic","NanumGothicOTF", gulim, "굴림";
+	}
+
+	.root_path {
+		margin: auto;
+		text-align: center;
+	}
+
+  </style>
+ </head>
+ <body>
+	<header>
+	</header>
+	<nav>
+	</nav>
+	<section>
+		<article>			
+			<h1 class='root_path'>aspirinirony</h1>
+		</article>
+	</section>
+	<footer>
+	</footer>
+ </body>
+</html>
+<?
+// Hosting : godo.com
+// DB HOST	localhost
+// DB NAME	aspirinirony_godohosting_com
+// DB 아이디	aspirinirony
+// phpadim http://aspirinirony.com/godo_mysql_phpmyadmin
+?>
